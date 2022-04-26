3813454c66500e29c68d0e9c9fb3c8e89e13cf6a
Aftab Sidhu <Aftab180@gmail.com>
2022-04-26 19:11:45 -0400

    commit tree 2ce8d32deee58475e0fce3a6f0e9dbe16ed7242b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(arr, string) {
+    const messages = [];
+    arr.forEach((name) => {
+        messages.push(`Thank you, ${name}, for the wonderful ${string} gift!`)
+    })
+    return messages;
+}
+
+function countDown(num) {
+    for(let i=0; i<=num; i++ ) {
+        console.log(i)
+    }
+}
